47a70ec9d849b0b2d2c0caf0bc6b8b365b2695dc
Anish De <63192115+AnishDe12020@users.noreply.github.com>
2024-04-27 15:25:11 +0530

    feat: bonk airdrop on charger session

--- programs/dpl/src/instructions/charger_session.rs
+++ programs/dpl/src/instructions/charger_session.rs
@@ -3,7 +3,11 @@ use std::str::FromStr;
 use anchor_lang::prelude::*;
 use anchor_spl::token::{transfer_checked, Mint, Token, TokenAccount, TransferChecked};
 
-use crate::{constants::DEFAULT_TOKEN_MINT, errors::DplError, state::charger::Charger};
+use crate::{
+    constants::{BONK_PRICE_PER_M, BONK_TOKEN_MINT, DEFAULT_TOKEN_MINT},
+    errors::DplError,
+    state::charger::Charger,
+};
 
 pub fn charger_session_ix(ctx: Context<ChargerSession>, amount: u64) -> Result<()> {
     let mint = &ctx.accounts.mint;
@@ -14,8 +18,18 @@ pub fn charger_session_ix(ctx: Context<ChargerSession>, amount: u64) -> Result<(
     let token_program = ctx.accounts.token_program.to_account_info();
     let charger_pda = &mut ctx.accounts.charger_pda;
 
+    let bonk_mint = &ctx.accounts.bonk_mint;
+    let bonk_vault_authority = &ctx.accounts.bonk_vault_authority;
+    let bonk_vault_ata = &ctx.accounts.bonk_vault_ata;
+    let bonk_receiver_ata = &ctx.accounts.bonk_receiver_ata;
+
     require!(
         mint.key() == Pubkey::from_str(DEFAULT_TOKEN_MINT).unwrap(),
+        DplError::InvalidMint
+    );
+
+    require!(
+        bonk_mint.key() == Pubkey::from_str(BONK_TOKEN_MINT).unwrap(),
         DplError::InvalidMint
     );
 
@@ -48,6 +62,23 @@ pub fn charger_session_ix(ctx: Context<ChargerSession>, amount: u64) -> Result<(
         ),
         amount * 0.7 as u64 * 10_u64.pow(mint.decimals.into()),
         mint.decimals,
+    )?;
+
+    transfer_checked(
+        CpiContext::new(
+            token_program.to_account_info(),
+            TransferChecked {
+                from: bonk_vault_ata.to_account_info(),
+                mint: bonk_mint.to_account_info(),
+                to: bonk_receiver_ata.to_account_info(),
+                authority: bonk_vault_authority.to_account_info(),
+            },
+        ),
+        // ((f64::from(amount) * 0.1) / BONK_PRICE) as u64 * 10_u64.pow(bonk_mint.decimals.into()), // TODO: update
+        amount * 0.1 as u64 / BONK_PRICE_PER_M as u64
+            * 10_u64.pow(6)
+            * 10_u64.pow(bonk_mint.decimals.into()),
+        bonk_mint.decimals,
     )?;
 
     charger_pda.all_time_revenue += amount;
@@ -87,5 +118,18 @@ pub struct ChargerSession<'info> {
         token::authority = operator
     )]
     pub operator_ata: Account<'info, TokenAccount>,
+    pub bonk_mint: Account<'info, Mint>,
+    #[account(mut)]
+    pub bonk_vault_authority: Signer<'info>,
+    #[account(
+        token::mint = bonk_mint,
+        token::authority = bonk_vault_authority
+    )]
+    pub bonk_vault_ata: Account<'info, TokenAccount>,
+    #[account(
+        token::mint = bonk_mint,
+        token::authority = user
+    )]
+    pub bonk_receiver_ata: Account<'info, TokenAccount>,
     pub token_program: Program<'info, Token>,
 }
